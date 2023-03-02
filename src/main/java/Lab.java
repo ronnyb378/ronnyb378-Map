1f9b5d941d326f86e865e86b50ad08e9e4b47d40
Ronny Barahona <ronnybarahona10@gmail.com>
2023-03-02 20:22:42 +0000

    hmm

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,4 @@
-
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -26,7 +26,7 @@ public class Lab {
      * so here Integer is a wrapper for int.
      */
     public Map<Integer, String> createMap(){
-        return null;
+        return new HashMap<Integer, String>();
     }
 
     /**
@@ -36,7 +36,7 @@ public class Lab {
      */
     public int getSize(Map<Integer,String> map){
 //        return total number of key/value pairs in the map
-        return 0;
+        return map.size();
     }
 
     /**
@@ -47,6 +47,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addKeyValuePair(Map<Integer,String> map, int key, String value){
+        map.put(key, value);
     }
 
     /**
@@ -56,7 +57,7 @@ public class Lab {
      * @return the value associated with key in map.
      */
     public String getValueFromKey(Map<Integer, String> map, int key){
-        return "";
+        return map.get(key);
     }
 
     /**
@@ -66,6 +67,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeKeyValuePair(Map<Integer, String> map, int key){
+        map.remove(key);
     }
 
     /**
@@ -76,6 +78,10 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void overwriteValue(Map<Integer, String> map, int key, String value){
-
+        if (map.containsKey(key)) {
+            map.put(key, value);
+        } else {
+            System.out.println("Key does not exist in map.");
+        }
     }
 }
